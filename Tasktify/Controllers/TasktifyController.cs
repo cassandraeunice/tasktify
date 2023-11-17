4cbf3c4bf9b3542b9448b34fd9e1796f8f93d82f
cc112 <cassandraeunice.cortez.cics@ust.edu.ph>
2023-11-18 02:15:52 +0800

    frontend medjo done

---
+++ Tasktify/Controllers/TasktifyController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Tasktify.Models;
+
+namespace Tasktify.Controllers
+{
+    public class TasktifyController : Controller
+    {
+        private readonly ILogger<TasktifyController> _logger;
+
+        public TasktifyController(ILogger<TasktifyController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult UserProfile()
+        {
+            return View();
+        }
+        
+        public IActionResult Todo()
+        {
+            return View();
+        }
+        public IActionResult Categories()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
